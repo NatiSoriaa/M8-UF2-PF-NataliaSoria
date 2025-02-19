49f0beedb50f75b02e0d03d8e4352f366ede1f72
NatiSoriaa <naticsoria@gmail.com>
2025-02-19 17:07:16 +0100

    Actualizacion PF

---
+++ api-express-mvc/models/MongoLibrary.js
@@ -0,0 +1,77 @@
+// const mysql = require("mysql2");
+const { MongoClient, ObjectId} = require("mongodb");
+
+const URL =  "mongodb://localhost:27017";
+const DB = "books";
+
+class Library {
+  constructor() {
+    this.client = new MongoClient(URL);
+  }  
+    async connect() {
+      try{
+        await this.client.connect();
+        this.database = this.client.db(DB);
+        this.collection = this.database.collection("books");
+        console.log("Successfully connected to the MongoDB database.");
+      }
+      catch(error){
+        console.error("Error connecting to MongoDB:", error);
+      }
+    }
+
+
+  close = async () => {
+    await this.client.close();
+  }
+
+  // métodos de la clase Library
+  listAll = async () => {
+    await this.connect();
+    const books = await this.collection.find({}).toArray();
+    console.log("fetched books", books);
+    await this.close();
+    return books;
+  }
+
+  create = async (newBook) => {
+    try {
+      await this.connect();
+      const result = await this.collection.insertOne(newBook);
+      return result.insertedId;
+    }
+    catch (error) {
+      return error;
+
+    }
+  };
+
+  update = async (bookID, updateBooks) => {
+
+    await this.connect();
+
+    const result = await this.collection.updateOne(
+      { _id: new ObjectId(String(bookID)) }, 
+      { $set:  updateBooks}
+    );
+
+    await this.close();
+    return result.modifiedCount;
+  }
+
+  delete = async (bookID) => {
+    try {
+        await this.connect();
+        const result = await this.collection.deleteOne({ _id: new ObjectId(String(bookID)) }); 
+        return result.deletedCount;
+    } catch (error) {
+        console.error("Error delete:", error);
+        return 0;
+    } finally {
+        await this.close();
+    }
+};
+
+}
+
+module.exports = Library;
